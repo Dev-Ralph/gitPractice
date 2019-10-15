474e674a611da8144acb2378c08593015c1affa3
Ralph Lopez <Ralph Lopez>
2019-10-15 21:44:16 +0800

    savem

--- resources/php/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['delete'])){
-require '../resources/config/connect.php';
-$sql = "DELETE FROM `customers` WHERE `customerName` = 'Lopez'";
-$data= $pdo->prepare($sql);
-$data->execute();
-$results = $data->fetchAll();
-}
-?>
